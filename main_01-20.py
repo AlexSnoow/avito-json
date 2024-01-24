5c09338d6c72128b71e345431f93dcd1097dd292
Alex Snow <t89521159005@gmail.com>
2024-01-24 12:53:43 +0200

    Попытка main_01-20.py получение json по URL - не удачно

--- main_01-20.py
+++ main_01-20.py
@@ -1,4 +1,31 @@
+import requests
 import json
+
+url = "https://www.avito.ru/web/1/js/items?locationId=629990&categoryId=98"
+import_file = "import.json"
+export_file = "export.json"
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+}
+
+try:
+    # Отправляем GET-запрос по указанной ссылке
+    response = requests.get(url, headers=headers)
+
+    # Проверяем успешность запроса
+    if response.status_code == 200:
+        # Получаем JSON из ответа
+        json_data = response.json()
+
+        # Записываем JSON в файл (с режимом перезаписи)
+        with open(import_file, "w", encoding="utf-8") as file:
+            json.dump(json_data, file, ensure_ascii=False, indent=2)
+        print(f"JSON успешно перезаписан в файл '{import_file}'")
+    else:
+        print(f"Ошибка при запросе: {response.status_code}")
+except Exception as e:
+    print(f"Произошла ошибка: {e}")
 
 
 def transform_item(input_item):
@@ -74,5 +101,5 @@ with open("import.json", "r", encoding="utf-8") as file:
 output_data = transform_json(input_data)
 
 # Записываем выходной JSON в файл
-with open("export.json", "w", encoding="utf-8") as file:
+with open(export_file, "w", encoding="utf-8") as file:
     json.dump(output_data, file, ensure_ascii=False, indent=2)
